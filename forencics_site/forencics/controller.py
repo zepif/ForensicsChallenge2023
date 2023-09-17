4b8d937b69755c1129133e1da5d4408500e61487
Oleksii Gamov <pepelover@fedora.fritz.box>
2023-09-17 23:03:28 +0200

    cleaned up website code

--- forencics_site/forencics/controller.py
+++ forencics_site/forencics/controller.py
@@ -1,362 +1,90 @@
 from dataclasses import dataclass
-from dataclasses import field
-from pathlib import Path
 from threading import RLock
-import time
-from typing import Dict
-from typing import List
-
-from forencics.configuration import QuestConfig
-from forencics.configuration import StageConfig
-from forencics.configuration import TaskConfig
-import yaml
 
 
 @dataclass
-class TaskCompletionInfo:
-    timestamp: float
-    time_since_stage_entered: float
-    points: int
-
-
-@dataclass
-class Message:
-    timestamp: float
-    text: str
-    safe: bool = False
-
-
-@dataclass
-class TeamInfo:
-    name: str
-    stage: int
-    stage_entered_timestamps: List[float]
-    total_points: int = 0
-    total_time: float = 0
-    finished: bool = False
-    completed_tasks: Dict[str, TaskCompletionInfo] = field(default_factory=dict)
-    messages: List[Message] = field(default_factory=list)
-
-
-@dataclass
-class AdminInfo:
-    name: str
-
-
-@dataclass
-class StandingsData:
-    team_order: List[str]
-    team_info: Dict[str, TeamInfo]
-    stages: List[StageConfig]
-    stage_points: Dict[str, List[int]]
-    stage_max_points: Dict[str, List[int]]
+class State:
+    placeholder: int
 
 
 class Controller:
-    """Main class for controlling the flow of the quest"""
+    """Main class for controlling the flow of the program"""
 
     def __init__(self):
         self.lock = RLock()
-        self.team_info: Dict[str, TeamInfo] = {}
-        self.admin_info: Dict[str, AdminInfo] = {}
-        self.is_game_started = False
 
-    def load(self, config: QuestConfig, event_log_path: Path):
-        """Loads the config and event log"""
-        self.config = config
-        self.event_log_path = event_log_path
-        self._load_event_log()
-        self._replay_event_log()
+    def load(self):
+        """Loads the config"""
 
-    def get_standings_data(self):
-        team_ids = [team.id for team in self.config.teams]
+    # def team_enter_code(self, team_id: str, code: str):
+    #     """
+    #     Team enters code
 
-        team_order = sorted(
-            team_ids,
-            key=lambda team_id: (
-                -self.team_info[team_id].total_points,
-                self.team_info[team_id].total_time,
-            ),
-        )
+    #     Return:
+    #         True if moved to next stage, else False
+    #     """
 
-        stage_points = {
-            team_id: [
-                sum(
-                    completion.points
-                    for task in stage.tasks
-                    if (
-                        completion := self.team_info[team_id].completed_tasks.get(
-                            task.id, None
-                        )
-                    )
-                    is not None
-                )
-                for stage in self.config.stages
-            ]
-            for team_id in team_ids
-        }
+    #     with self.lock:
+    #         timestamp = time.time()
 
-        stage_max_points = {
-            team_id: [
-                sum(task.max_points for task in stage.tasks)
-                for stage in self.config.stages
-            ]
-            for team_id in team_ids
-        }
+    #         self._log_event(
+    #             "team_enter_code",
+    #             team_id=team_id,
+    #             code=code,
+    #             timestamp=timestamp,
+    #         )
 
-        return StandingsData(
-            team_order=team_order,
-            team_info=self.team_info,
-            stages=self.config.stages,
-            stage_points=stage_points,
-            stage_max_points=stage_max_points,
-        )
+    #         return self._team_enter_code(
+    #             team_id=team_id,
+    #             code=code,
+    #             timestamp=timestamp,
+    #         )
 
-    def get_team_info(self, team_id: str):
-        """Returns info for a given team"""
-        return self.team_info[team_id]
+    # def _team_enter_code(self, team_id: str, code: str, timestamp: float):
+    #     """Enter code logic"""
 
-    def get_admin_info(self, admin_id: str):
-        """Returns info for a given admin"""
-        return self.admin_info[admin_id]
+    #     team_info = self.team_info[team_id]
 
-    def team_login(self, team_id: str, password: str):
-        for team in self.config.teams:
-            if team_id == team.id and password == team.password:
-                return True
-        return False
+    #     if team_info.finished:
+    #         return False
 
-    def admin_login(self, admin_id: str, password: str):
-        for admin in self.config.admins:
-            if admin_id == admin.id and password == admin.password:
-                return True
-        return False
+    #     stage = self.config.stages[team_info.stage]
+    #     time_since_stage_entered = timestamp - team_info.stage_entered_timestamps[-1]
 
-    def up_stage_if_needed(self, team_id: str, timestamp: float):
-        """
-        Moves the team to next stage if required (i.e. timeout or all tasks completed)
+    #     code_accepted = False
 
-        Returns:
-            True if moved to next stage, else False
-        """
-        with self.lock:
-            team_info = self.team_info[team_id]
+    #     for task in stage.tasks:
+    #         if task.code == code and task.id not in team_info.completed_tasks:
+    #             points = self._get_points_for_completed_task(
+    #                 task_config=task,
+    #                 time_since_stage_entered=time_since_stage_entered,
+    #             )
 
-            if team_info.finished:
-                return
+    #             team_info.total_points += points
+    #             if task.counts_towards_total_time:
+    #                 team_info.total_time += time_since_stage_entered
 
-            stage = self.config.stages[team_info.stage]
-            stage_entered_time = team_info.stage_entered_timestamps[-1]
+    #             team_info.completed_tasks[task.id] = TaskCompletionInfo(
+    #                 timestamp=timestamp,
+    #                 time_since_stage_entered=time_since_stage_entered,
+    #                 points=points,
+    #             )
 
-            if timestamp >= stage_entered_time + stage.duration:
-                # Time on stage ended
-                self._up_stage(
-                    team_info=team_info,
-                    timestamp=stage_entered_time + stage.duration,
-                    timeout=True,
-                )
-                return True
+    #             code_accepted = True
 
-            elif all(task.id in team_info.completed_tasks for task in stage.tasks):
-                # All tasks completed
-                self._up_stage(
-                    team_info=team_info,
-                    timestamp=timestamp,
-                    timeout=False,
-                )
-                return True
+    #     message_text = f"Введено код: {code}. "
+    #     if code_accepted:
+    #         message_text += "Код зараховано!"
+    #     else:
+    #         message_text += "Неправильний код!"
 
-            return False
+    #     team_info.messages.append(
+    #         Message(
+    #             timestamp=timestamp,
+    #             text=message_text,
+    #         )
+    #     )
 
-    def game_start(self):
-        """
-        Starts the game
-        """
+    #     stage_upped = self.up_stage_if_needed(team_id=team_id, timestamp=timestamp)
 
-        with self.lock:
-            timestamp = time.time()
-
-            self._log_event(
-                "game_start",
-                timestamp=timestamp,
-            )
-
-            self._game_start(timestamp=timestamp)
-
-    def admin_start(self):
-        self.admin_info = {
-            admin.id: AdminInfo(name=admin.name) for admin in self.config.admins
-        }
-
-    def team_enter_code(self, team_id: str, code: str):
-        """
-        Team enters code
-
-        Return:
-            True if moved to next stage, else False
-        """
-
-        with self.lock:
-            timestamp = time.time()
-
-            self._log_event(
-                "team_enter_code",
-                team_id=team_id,
-                code=code,
-                timestamp=timestamp,
-            )
-
-            return self._team_enter_code(
-                team_id=team_id,
-                code=code,
-                timestamp=timestamp,
-            )
-
-    def _log_event(self, event_name: str, timestamp: float, **kwargs):
-        """Logs event"""
-
-        self.event_log.append(
-            {
-                "event": event_name,
-                "timestamp": timestamp,
-                "params": kwargs,
-            }
-        )
-
-        self._dump_event_log()
-
-    def _load_event_log(self):
-        """Loads event log from json file"""
-
-        if not self.event_log_path.exists():
-            self.event_log = []
-            self._dump_event_log()
-        else:
-            with open(self.event_log_path) as f:
-                self.event_log = yaml.full_load(f)
-                if self.event_log is None:
-                    self.event_log = []
-
-    def _dump_event_log(self):
-        """Dumps event log to json file"""
-
-        with open(self.event_log_path, "w") as f:
-            yaml.dump(self.event_log, f)
-
-    def _replay_event_log(self):
-        """Replays all the events of the quest from log"""
-        MAPPING = {
-            "team_enter_code": self._team_enter_code,
-            "game_start": self._game_start,
-        }
-        for event in self.event_log:
-            MAPPING[event["event"]](timestamp=event["timestamp"], **event["params"])
-
-    def _game_start(self, timestamp: float):
-        self.team_info = {
-            team.id: TeamInfo(
-                name=team.name, stage=0, stage_entered_timestamps=[timestamp]
-            )
-            for team in self.config.teams
-        }
-        self.is_game_started = True
-
-    def _team_enter_code(self, team_id: str, code: str, timestamp: float):
-        """Enter code logic"""
-
-        team_info = self.team_info[team_id]
-
-        if team_info.finished:
-            return False
-
-        stage = self.config.stages[team_info.stage]
-        time_since_stage_entered = timestamp - team_info.stage_entered_timestamps[-1]
-
-        code_accepted = False
-
-        for task in stage.tasks:
-            if task.code == code and task.id not in team_info.completed_tasks:
-                points = self._get_points_for_completed_task(
-                    task_config=task,
-                    time_since_stage_entered=time_since_stage_entered,
-                )
-
-                team_info.total_points += points
-                if task.counts_towards_total_time:
-                    team_info.total_time += time_since_stage_entered
-
-                team_info.completed_tasks[task.id] = TaskCompletionInfo(
-                    timestamp=timestamp,
-                    time_since_stage_entered=time_since_stage_entered,
-                    points=points,
-                )
-
-                code_accepted = True
-
-        message_text = f"Введено код: {code}. "
-        if code_accepted:
-            message_text += "Код зараховано!"
-        else:
-            message_text += "Неправильний код!"
-
-        team_info.messages.append(
-            Message(
-                timestamp=timestamp,
-                text=message_text,
-            )
-        )
-
-        stage_upped = self.up_stage_if_needed(team_id=team_id, timestamp=timestamp)
-
-        return stage_upped
-
-    @staticmethod
-    def _get_points_for_completed_task(
-        task_config: TaskConfig, time_since_stage_entered: float
-    ):
-        return int(
-            task_config.max_points
-            * sum(
-                p.percent / 100
-                for p in task_config.scoring
-                if p.completed_before >= time_since_stage_entered
-            )
-        )
-
-    def _up_stage(self, team_info: TeamInfo, timestamp: float, timeout: bool):
-        team_info.stage += 1
-        team_info.stage_entered_timestamps.append(timestamp)
-        if team_info.stage == len(self.config.stages):
-            team_info.finished = True
-            team_info.messages.append(
-                Message(
-                    timestamp=timestamp,
-                    text="Ви прийшли на фініш!",
-                )
-            )
-        else:
-            if timeout:
-                team_info.messages.append(
-                    Message(
-                        timestamp=timestamp,
-                        text=f"Час на етапі вийшов. Ви перейшли на етап {team_info.stage}.",
-                    )
-                )
-            else:
-                team_info.messages.append(
-                    Message(
-                        timestamp=timestamp,
-                        text=f"Всі завдання виконано. Ви перейшли на етап {team_info.stage}.",
-                    )
-                )
-
-            message = self.config.stages[team_info.stage].message
-
-            if message is not None:
-                team_info.messages.append(
-                    Message(
-                        timestamp=timestamp,
-                        text=message,
-                        safe=True,
-                    )
-                )
+    #     return stage_upped
--- forencics_site/forencics/server.py
+++ forencics_site/forencics/server.py
@@ -1,20 +1,12 @@
 from pathlib import Path
-import time
 
 import click
 from flask import Flask
 from flask import redirect
 from flask import render_template
-from flask import request
-from flask import session
 from flask import url_for
-from forencics.filters import register_filters
-from forencics.configuration import QuestConfig
 from forencics.controller import Controller
-from forencics.utils import synchronized
-from omegaconf import OmegaConf
 import yaml
-
 
 
 app = Flask(__name__)
@@ -33,7 +25,6 @@ def main_page():
 
 
 @app.route("/model", methods=["GET", "POST"])
-@synchronized(controller.lock)
 def model_page():
     # TODO add 3d model and make it spin
     return render_template(
@@ -54,12 +45,14 @@ def pres_page():
         "pres.html",
     )
 
+
 @app.route("/info")
 def info_page():
     # TODO add CVs and everything else
     return render_template(
         "info.html",
     )
+
 
 @click.option(
     "-c",
@@ -68,35 +61,14 @@ def info_page():
     type=click.Path(dir_okay=False, exists=True, path_type=Path),
 )
 @click.option(
-    "-e",
-    "--event_log_path",
-    type=click.Path(dir_okay=False, path_type=Path),
-    default=Path("events.yaml"),
-)
-@click.option(
     "-p",
     "--port",
     type=int,
     default=5000,
 )
-
-def run_server(config_path: Path, event_log_path: Path, port: int):
+def run_server(config_path: Path, port: int):
     with open(config_path) as f:
         config = yaml.full_load(f)
-        schema = OmegaConf.create(QuestConfig())
-        quest_config = OmegaConf.merge(schema, config["quest"])
-
-    controller.load(config=quest_config, event_log_path=event_log_path)
-
-    controller.admin_start()
-
-    if not controller.is_game_started:
-        controller.game_start()
-
-    app.config["SECRET_KEY"] = config["flask_secret_key"]
-
-    register_filters(app)
-
     app.run(host="0.0.0.0", port=port, debug=True, extra_files=[config_path])
 
 
