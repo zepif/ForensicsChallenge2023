de0a95e6ac08073212ff0fc33d28cb2ba6564fef
zepif <zellti152@gmail.com>
2023-09-22 22:19:02 +0300

    trying to make voice2text

--- audio_detection/speech_recognition_fromscratch/rec.py
+++ audio_detection/speech_recognition_fromscratch/rec.py
@@ -1,7 +1,7 @@
 import os
 import csv
 import torch
-from tqdm.auto import tqdm
+from tqdm import tqdm
 from torch import nn
 import torch.optim as optim
 import torchaudio
@@ -12,6 +12,7 @@ DATASET = "C:\\fun\ForensicsChallenge2023_team6\\audio_detection\speech_recognit
 CHARSET = " abcdefghijklmnopqrstuvwxyz,."
 XMAX = 810
 YMAX = 150
+SAMPLE_RATE = 22050
 
 def get_metadata():
     ret = []
@@ -24,19 +25,21 @@ def get_metadata():
     print("got metadata", len(ret))
     return ret
 
+mel_transform = torchaudio.transforms.MelSpectrogram(SAMPLE_RATE, n_fft=1024, win_length=1024,
+                                                              hop_length=1024, n_mels=80)
 def load_example(x):
     waveform, sample_rate = torchaudio.load(x, normalize=True)
-    assert(sample_rate == self.sample_rate)
-    mel_specgram = self.transform(waveform)
+    if sample_rate != SAMPLE_RATE:
+        print(f"Sample rate mismatch in file {x}. Expected {SAMPLE_RATE}, but got {sample_rate}.")
+    assert(sample_rate == SAMPLE_RATE)
+    mel_specgram = mel_transform(waveform)
     return mel_specgram[0].T
 
 cache = {}
 class LJSpeech(Dataset):
     def __init__(self):
         self.meta = get_metadata()
-        self.sample_rate = 22050
-        self.transform = torchaudio.transforms.MelSpectrogram(self.sample_rate, n_fft=1024, win_length=1024,
-                                                              hop_length=1024, n_mels=80)
+        #self.sample_rate = 22050
 
     def __len__(self):
         return len(self.meta)
@@ -44,25 +47,46 @@ class LJSpeech(Dataset):
     def __getitem__(self, idx):
         if idx not in cache:
             x, y = self.meta[idx]
-            #return 10+torch.log10(mel_specgram[0]).T, y
-            cache[idx] = x, y
+            cache[idx] = load_example(x), y
         return cache[idx]
     
+class GoodBatchNorm(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.bn = nn.BatchNorm1d(channels)
+    
+    def forward(self, x):
+        #return self.bn(x.permute(1, 2, 0)).permute(1, 2, 0)
+        #return x
+        xx = x.permute(1, 2, 0)
+        xx = self.bn(xx)
+        xx = xx.permute(2, 0, 1)
+        return xx
+
 class Rec(nn.Module):
     def __init__(self):
         super().__init__()
+        # (L, N, C)
+        H = 256
         self.prepare = nn.Sequential(
-            nn.Linear(80, 120),
+            nn.Linear(80, H),
+            GoodBatchNorm(H),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(H, H),
+            GoodBatchNorm(H),
             nn.ReLU()
         )
-        self.encoder = nn.GRU(128, 128, batch_first=False)
-        self.decode = nn.Linear(128, len(CHARSET))
+        self.encoder = nn.GRU(H, H, batch_first=False)
+        self.decode = nn.Sequential(
+            nn.Linear(H, H//2),
+            GoodBatchNorm(H//2),
+            nn.ReLU(),
+            nn.Linear(H//2, len(CHARSET))
+        )
     
     def forward(self, x):
         x = self.prepare(x)
-        x = nn.fuctional_relu(self.encoder(x)[0])
+        x = nn.functional.relu(self.encoder(x)[0])
         x = self.decode(x)
         return torch.nn.functional.log_softmax(x, dim=2)
     
@@ -71,19 +95,34 @@ def pad_sequence(batch):
     input_lengths = [x[0].shape[0] for x in sorted_batch]
     target_lengths = [len(x[1]) for x in sorted_batch]
     sequences = [x[0] for x in sorted_batch]
-    sequences_padded = torch.nn.utils.rnn.pad_sequence(sequences, batch_first=False)
-    labels = [x[1] + [0]*[YMAX - len(x[1])] for x in sorted_batch]
-    labels = torch.LongTensor(labels)
-    return sequences_padded, labels[:, :max(target_lengths)], input_lengths, target_lengths
+    sequences_padded = torch.nn.utils.rnn.pad_sequence(sequences, batch_first=True)
+    # YMAX = max(input_length)
+    #labels = [x[1] + [0]*(YMAX - len(x[1])) for x in sorted_batch]
+    #labels = torch.LongTensor(labels)   
+    #labels = labels[:, :max(target_lengths)]
+    labels = sum([x[1] for x in sorted_batch], [])
+    labels = torch.tensor(labels, dtype=torch.int32)
+    return sequences_padded, labels, input_lengths, target_lengths
 
 def get_dataloader(batch_size):
     dset = LJSpeech()
-    trainloader = torch.utils.data.DataLoader(dset, batch_size=batch_size, shuffle=True, num_workers=8, 
-                                                collate_fn=None) #
+    trainloader = torch.utils.data.DataLoader(dset, batch_size=batch_size, shuffle=True, 
+                                                collate_fn=pad_sequence, drop_last=True) #
     return dset, trainloader
 
-def train(): 
-    batch_size=32
+import wandb
+
+WAN = False
+
+def train():
+    batch_size = 64
+    if WAN:
+        wandb.init(project="speech", entity="zellti152") 
+    config={
+        "learning_rate": 0.001,
+        "epochs": 100,
+        "batch_size": batch_size,
+    }
     dset, trainloader = get_dataloader(batch_size)
     #ctc_loss = nn.CTCLoss(reduction='mean', zero_infinity='True').cuda()
     #model = Rec().cuda()
@@ -91,21 +130,42 @@ def train():
     model = Rec()
     #optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+    val = torch.tensor(load_example('data/wavs/LJ037-0171.wav'))
     for epoch in range(100):
+        if WAN:
+            wandb.watch(model)
+        mguess = model(val[:, None])
+        pp = ''.join([CHARSET[c-1] for c in mguess[:, 0, :].argmax(dim=1) if c != 0])
+        print('Validation: ',  pp)
+        torch.save(model.state_dict(), f'./models/speech_{epoch}.pt')
+
         t = tqdm(trainloader, total=len(dset)//batch_size)
+        ind = 0
         for data in t:
             input, target, input_length, target_length = data
+            input_length = torch.as_tensor(input_length)
+            target_length = torch.as_tensor(target_length)
+
             #input = input.cuda()
+            #input = input.to('cuda:0', non_blocking=True)
             #target = target.cuda()
+            #target = target.to('cuda:0', non_blocking=True)
             optimizer.zero_grad()
             guess = model(input)
-            pp = ''.join([CHARSET(c-1) for c in guess[:, 0, :].argmax(dim=1).cpu() if c != 0])
+
+            pp = ''.join([CHARSET[c-1] for c in guess[:, 0, :].argmax(dim=1) if c != 0])
             if len(pp) > 0:
                 print(pp)
+            #print(input_length.type())
+            #print(f'guess : {guess} ; target : {target} ; input size {input_length} ; target size : {target_length}')
             loss = ctc_loss(guess, target, input_length, target_length)
+            ind = ind + 1
             loss.backward()
             optimizer.step()
             t.set_description("loss: %.2f" % loss.item())
+            if WAN:
+                wandb.log({"loss": loss})
 
 if __name__ == '__main__':
     train()
+
